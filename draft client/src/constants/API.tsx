20ce26a4971137f23ce8d5b01c8d75feb9c9e91f
unknown <deansh91@gmail.com>
2023-05-31 03:12:38 +0300

    Update server URL

--- draft client/src/constants/API.tsx
+++ draft client/src/constants/API.tsx
@@ -1,5 +1,5 @@
-export const server = 'http://localhost:8080';
-export const serverEmitter = 'http://localhost:8080';
+// export const server = 'http://localhost:8080';
+// export const serverEmitter = 'http://localhost:8080';
 
-// export const server = 'http://52.50.28.56:8080';
-// export const serverEmitter = 'http://52.50.28.56:8080';
+export const server = 'http://34.243.129.70:8080';
+export const serverEmitter = 'http://34.243.129.70:8080';
