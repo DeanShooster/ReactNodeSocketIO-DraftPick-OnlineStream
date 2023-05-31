f3ba5ce7e0254cd3de207cf1859e510c5658d55c
unknown <deansh91@gmail.com>
2023-05-31 03:17:27 +0300

    Update server URL

--- streamer-client/src/constants/API.ts
+++ streamer-client/src/constants/API.ts
@@ -1,3 +1,6 @@
 
-export const server = 'http://localhost:8080';
-export const serverEmitter = 'http://localhost:8080';
+// export const server = 'http://localhost:8080';
+// export const serverEmitter = 'http://localhost:8080';
+
+export const server = 'http://34.243.129.70:8080';
+export const serverEmitter = 'http://34.243.129.70:8080';
